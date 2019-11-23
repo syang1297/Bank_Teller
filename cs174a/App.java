1b890c011a49f45f9b23d7df1165493d9813a241
Andrew Doan <andrewdoancs@gmail.com>
2019-11-22 22:07:55 -0800

    set up

--- cs174a/App.java
+++ cs174a/App.java
@@ -54,8 +54,8 @@ public class App implements Testable
 	{
 		// Some constants to connect to your DB.
 		final String DB_URL = "jdbc:oracle:thin:@cs174a.cs.ucsb.edu:1521/orcl";
-		final String DB_USER = "c##YoutNetID";
-		final String DB_PASSWORD = "YourPassword";
+		final String DB_USER = "c##andrewdoan";
+		final String DB_PASSWORD = "377365";
 
 		// Initialize your system.  Probably setting up the DB connection.
 		Properties info = new Properties();
